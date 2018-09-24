d95bcc546abdea690499a2cd42fa32aca3a59636
TVIGes <khdv66@gmail.com>
2018-09-25 00:07:51 +0500

    Additional tests

---
+++ test/com/onedayfirm/gorodabot/goroda/FileCitiesStorageTest.java
@@ -0,0 +1,35 @@
+package com.onedayfirm.gorodabot.goroda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileCitiesStorageTest {
+
+    @Test
+    void getInstance() {
+        var storage1 = FileCitiesStorage.getInstance();
+        var storage2 = FileCitiesStorage.getInstance();
+
+        assertNotNull(storage1);
+        assertNotNull(storage2);
+        assertEquals(storage1, storage2);
+    }
+
+    @Test
+    void getCitiesByLetterValid() {
+        var storage = FileCitiesStorage.getInstance();
+        var cities = storage.getCitiesByLetter('ю');
+
+        assertNotNull(cities);
+        assertNotEquals(0, cities.size());
+    }
+
+    @Test
+    void getCitiesByLetterInvalid() {
+        var storage = FileCitiesStorage.getInstance();
+        var cities = storage.getCitiesByLetter('\'');
+
+        assertNull(cities);
+    }
+}
