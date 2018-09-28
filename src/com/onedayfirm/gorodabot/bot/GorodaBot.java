10cff12c7174a4028c6340b07b8b9ba4c6e2dab5
TVIGes <khdv66@gmail.com>
2018-09-29 01:13:54 +0500

    Add help command

--- src/com/onedayfirm/gorodabot/bot/GorodaBot.java
+++ src/com/onedayfirm/gorodabot/bot/GorodaBot.java
@@ -13,7 +13,7 @@ public class GorodaBot implements Bot {
     public GorodaBot() {
         sessions = new HashMap<>();
     }
-    
+
     public boolean isUserConnected(int id) {
         return sessions.containsKey(id);
     }
@@ -21,37 +21,45 @@ public class GorodaBot implements Bot {
     public void onConnection(int id, Collection<String> responses) {
         sessions.put(id, new Session());
         responses.add(phrases.getPhrase("GREETING"));
+        responses.add(phrases.getPhrase("ABOUT"));
         responses.add(phrases.getPhrase("HELP"));
     }
 
     public void onMessage(int id, String message, Collection<String> responses) {
         var session = sessions.get(id);
-        if (message.equals("/game")) {
+        if (message.equals("/help")) {
+            responses.add(phrases.getPhrase("ABOUT"));
+            responses.add(phrases.getPhrase("HELP"));
+        } else if (message.equals("/game")) {
             var game = new GorodaGame();
             session.setGorodaGame(game);
             responses.add(game.makeFirstTurn());
         } else if (session.getGorodaGame() != null) {
-            if (!session.getGorodaGame().isValidCity(message)) {
-                responses.add(phrases.getPhrase("UNKNOWN CITY"));
-                return;
-            }
-            if (!session.getGorodaGame().isCorrectTurn(message)) {
-                responses.add(phrases.getPhrase("WRONG ANSWER"));
-                return;
-            }
-            if (session.getGorodaGame().isCityUsed(message)) {
-                responses.add(phrases.getPhrase("CITY ALREADY USED"));
-                return;
-            }
-            var city = session.getGorodaGame().makeTurn(message);
-            if (city == null) {
-                responses.add(phrases.getPhrase("LOSE"));
-                session.setGorodaGame(null);
-            } else
-                responses.add(city);
+            handleGorodaGameTurn(session, message, responses);
         } else {
             responses.add(phrases.getPhrase("UNKNOWN COMMAND"));
             responses.add(phrases.getPhrase("HELP"));
         }
+    }
+
+    private void handleGorodaGameTurn(Session session, String message, Collection<String> responses) {
+        if (!session.getGorodaGame().isValidCity(message)) {
+            responses.add(phrases.getPhrase("UNKNOWN CITY"));
+            return;
+        }
+        if (!session.getGorodaGame().isCorrectTurn(message)) {
+            responses.add(phrases.getPhrase("WRONG ANSWER"));
+            return;
+        }
+        if (session.getGorodaGame().isCityUsed(message)) {
+            responses.add(phrases.getPhrase("CITY ALREADY USED"));
+            return;
+        }
+        var city = session.getGorodaGame().makeTurn(message);
+        if (city == null) {
+            responses.add(phrases.getPhrase("LOSE"));
+            session.setGorodaGame(null);
+        } else
+            responses.add(city);
     }
 }
